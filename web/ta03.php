ff7e889e39241bb5cbf8f2775ea88ebac69033a9
Mayeda-Mark <markrmayeda@gmail.com>
2018-10-03 20:01:30 -0700

    commited stuff

---
+++ web/ta03.php
@@ -0,0 +1,19 @@
+<?php
+//ta02.php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$major = $_POST['major'];
+$comments = $_POST['comment'];
+
+
+
+echo "<h1>Your Input:</h1>"
+echo $name;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $major;
+echo "<br>";
+echo $comment;
+
+?>

e487cef05aa7b325ee0960d74825289451532365
Mayeda-Mark <markrmayeda@gmail.com>
2018-10-05 10:55:53 -0700

    initial commit

---
+++ web/footer.php
@@ -0,0 +1,3 @@
+<?php
+echo "<p>Neither Mark's Cat Emporium nor its parent company, MarkCo are not legally responsible for any damages, physical, emotional or otherwise to property or persons done by any cat, man dressed as a cat or hellspawn in cat form purchased from Mark's cat Emporium.</p>";
+?>

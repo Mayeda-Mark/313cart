50cb450a586b5a7edbf9d28589f6c5793367ff4c
Mayeda-Mark <markrmayeda@gmail.com>
2018-10-05 19:48:24 -0700

    testing submit

---
+++ web/cart.js
@@ -0,0 +1,8 @@
+//cart.js
+
+function addToCart(name, price) {
+	var txt = "<?php $_SESSION['" + name +"'] = '"
+	+ name "'; $_SESSION['" + name + "price'] = '" 
+	+ price + "'; ?>";
+	document.getElementById('emptySpace').innerHTML = txt;
+}

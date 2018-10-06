404bb9414236243a48479247ded76c3d5d85e423
Mayeda-Mark <markrmayeda@gmail.com>
2018-10-05 19:40:17 -0700

    testing submit

--- web/browse.php
+++ web/browse.php
@@ -26,8 +26,8 @@ session_start();
 			</tr>
 			<tr>
 				<td>
-					<form id="mrButtons" action="">
-						<input type="submit" value="Add to Cart" " form="mrButtons" >
+					<form id="mrButtons" method="post" action="">
+						<input type="button" value="Add to Cart" onclick="addToCart('Mr. Buttons', '$50')" >
 				</form></td>
 				<td>Mr. Buttons</td>
 				<td><img src="https://i.imgur.com/eADvu2n.jpg" width="150" height="125" /></td>
@@ -36,8 +36,8 @@ session_start();
 			</tr>
 			<tr>
 				<td>
-					<form id="freddyMeowcurry" action="">
-						<input type="submit" src="https://i.imgur.com/pea2Bth.png"  width="100" height="75" form="freddyMeowcurry">
+					<form id="freddyMeowcurry" method="post" action="">
+						<input type="submit" value="Add to Cart" form="freddyMeowcurry">
 					</form></td>
 				<td>"Freddy Meowcurry"</td>
 				<td><img src="https://i.imgur.com/AGThVJj.jpg" width="150" height="125"/></td>
@@ -46,8 +46,8 @@ session_start();
 			</tr>
 			<tr>
 				<td>
-					<form id="calicoJack" action="">
-						<input type="submit" src="https://i.imgur.com/pea2Bth.png"  width="100" height="75" form="calicoJack">
+					<form id="calicoJack" method="post" action="">
+						<input type="submit" value="Add to Cart" form="calicoJack">
 					</form></td>
 				<td>Calico Jack</td>
 				<td><img src="https://i.imgur.com/2jbNmgN.jpg" height="125" width="150" /></td>
@@ -55,55 +55,82 @@ session_start();
 				<td>Calico Jack is the bravest cat to ever sail the seven seas.  With his trusted parrot, Crackers, he is always on the lookout for adventure.  Also, he has diabetes.</td>
 			</tr>
 			<tr>
-				<td><img src="https://i.imgur.com/pea2Bth.png" title="source: imgur.com" width="100" height="75"/></td>
+				<td>
+					<form id="spock" method="post" action="">
+						<input type="submit" value="Add to Cart" form="spock">
+					</form></td>
 				<td>Spock</td>
-				<td><img src="https://i.imgur.com/O8RxLUb.jpg" title="source: imgur.com" width="150" height="125" /></td>
+				<td><img src="https://i.imgur.com/O8RxLUb.jpg" width="150" height="125" /></td>
 				<td>$50</td>
 				<td>Spock is a cat who likes to scratch web developers who are in charge of making cat sales websites.</td>
 			</tr>
 			<tr>
-				<td><img src="https://i.imgur.com/pea2Bth.png" title="source: imgur.com" width="100" height="75"/></td>
+				<td>
+					<form id="astoroth" method="post" action="">
+						<input type="submit" value="Add to Cart" form="astoroth">
+					</form></td>
 				<td>Astoroth, the World Breaker</td>
-				<td><img src="https://i.imgur.com/7t2X5tC.jpg" title="source: imgur.com" width="150" height="125" /></td>
+				<td><img src="https://i.imgur.com/7t2X5tC.jpg" width="150" height="125" /></td>
 				<td>$75</td>
 				<td>Astoroth, the World Breaker is a lovely black cat who loves to cuddle and sit in the sun.  He claims that he's here to bring about the end times outlined in the book of Revelation, but he's a really sweet boy.</td>
 			</tr>
 			<tr>
-				<td><img src="https://i.imgur.com/pea2Bth.png" title="source: imgur.com" width="100" height="75"/></td>
+				<td>
+					<form id="paul" method="post" action="">
+						<input type="submit" value="Add to Cart" form="paul">
+					</form></td>
 				<td>Paul</td>
-				<td><img src="https://i.imgur.com/aEwwYYG.jpg" title="source: imgur.com" width="125" height="125" /></td>
+				<td><img src="https://i.imgur.com/aEwwYYG.jpg" width="125" height="125" /></td>
 				<td>$20</td>
 				<td>Paul is a cat who likes doing cat things.  He eats cat food and listens to cat music.</td>
 			</tr>
 			<tr>
-				<td><img src="https://i.imgur.com/pea2Bth.png" title="source: imgur.com" width="100" height="75"/></td>
+				<td>
+					<form id="brush" method="post" action="">
+						<input type="submit" value="Add to Cart" form="brush">
+					</form></td>
 				<td>Licki Brush</td>
-				<td><img src="https://i.imgur.com/2EyL6p3.jpg" title="source: imgur.com" width="150" height="125" /></td>
+				<td><img src="https://i.imgur.com/2EyL6p3.jpg" width="150" height="125" /></td>
 				<td>$25</td>
 				<td>Have you ever watched your cat lick another cat and though to yourself "I wish that I could do that without getting fur in my mouth"?  If so, the Licki Brush is just the product for you.  With this patented brush, you can groom your cat while pretending to lick it, because that's a normal thing to do.</td>
 			</tr>
 			<tr>
-				<td><img src="https://i.imgur.com/pea2Bth.png" title="source: imgur.com" width="100" height="75"/></td>
+				<td>
+					<form id="wheel" method="post" action="">
+						<input type="submit" value="Add to Cart" form="wheel">
+					</form></td>
 				<td>Cat Exercise Wheel</td>
-				<td><img src="https://i.imgur.com/LL3wOWR.jpg" title="source: imgur.com" width="150" height="125" /></td>
+				<td><img src="https://i.imgur.com/LL3wOWR.jpg" width="150" height="125" /></td>
 				<td>$50</td>
 				<td>You cat will love this cat-sized exercise wheel.  Everyone with a cat will tell you that cats don't ever walk around on their own, so this is the perfect item to help your cat keep in shape, and honestly, it needs it after that nasty divorce it's just been through.</td>
 			</tr>
 			<tr>
-				<td><img src="https://i.imgur.com/pea2Bth.png" title="source: imgur.com" width="100" height="75"/></td>
+				<td>
+					<form id="carrier" method="post" action="">
+						<input type="submit" value="Add to Cart" form="carrier">
+					</form></td>
 				<td>U-Pet Carrier</td>
-				<td><img src="https://i.imgur.com/QWtoZ2S.jpg" title="source: imgur.com" width="125" height="125" /></td>
+				<td><img src="https://i.imgur.com/QWtoZ2S.jpg" width="125" height="125" /></td>
 				<td>$35</td>
 				<td>With this pet carrier, you can show your cat the wonders of the world without worying about it escaping or getting into a fight with any other cats whos territory you may wander into.</td>
 			</tr>
 			<tr>
-				<td><img src="https://i.imgur.com/pea2Bth.png" title="source: imgur.com"width="100" height="75" /></td>
+				<td>
+					<form id="mittons" method="post" action="">
+						<input type="submit" value="Add to Cart" form="mittons">
+					</form></td>
 				<td>Kitten Mittons</td>
-				<td><img src="https://i.imgur.com/Zjm0949.jpg" title="source: imgur.com" width="150" height="125" /></td>
+				<td><img src="https://i.imgur.com/Zjm0949.jpg" width="150" height="125" /></td>
 				<td>$20</td>
 				<td> Is your cat making TOO MUCH NOISE all the time? Is your cat constantly stomping around driving you crazy? Is your cat clawing at your furnitures? Think there’s no answer? You’re so stupid. There is! Kitten Mittons! Finally there’s an elegant, comfortable mitten for cats! Is your cat one legged? Is your cat fat, skinny or an in between? That doesn’t matter ‘cuz one size fits all! Kitten Mittons, you’ll be smitten. Meee-owww!</td>
 			</tr>
 		</table>
+		<div id="emptySpace"></div>
+		<div id="test">
+			<?php
+			print_r($_SESSION);
+			?>
+		</div>
 	<div class="footer">
 		<?php 
 		include 'footer.php'
--- web/home.php
+++ web/home.php
@@ -12,7 +12,7 @@
 	</div>
 	<h1>Home</h1>
 	<p>Welcome to Mark's Cat Emporium!  Home for all of your cats and cat needs.  Click the link below to see all of our amazing cats and products!</p>
-	<a href="browse.html">Click Here!</a>
+	<a href="browse.php">Click Here!</a>
 	<div class="footer">
 		<?php 
 		include 'footer.php';

8aff76fd489737616b743391a399b6f1c46890b4
Mayeda-Mark <markrmayeda@gmail.com>
2018-10-09 15:21:20 -0700

    closing in on the end

--- web/browse.php
+++ web/browse.php
@@ -24,7 +24,7 @@ session_start();
 	</div>
 	<h1>Click on an item's checkbox to add them to your cart</h1>
 	<form id="cart" method="post" action="viewCart.php">
-		<table style='border: 1px solid black';>
+		<table style="border: 1px solid black";>
 			<tr>
 				<th></th>
 				<th>Item</th>

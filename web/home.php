99cbbcbc519b9e7df23007afa9df32e29971ed8b
Mayeda-Mark <markrmayeda@gmail.com>
2018-10-09 15:23:58 -0700

    closing in on the end

--- web/home.php
+++ web/home.php
@@ -14,7 +14,7 @@
 	<p>Welcome to Mark's Cat Emporium!  Home for all of your cats and cat needs.  Click the link below to see all of our amazing cats and products!</p>
 	<a href="browse.php">Click Here!</a>
 	
-	<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+	<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 	<div class="footer">
 		<?php 
 		include 'footer.php';
